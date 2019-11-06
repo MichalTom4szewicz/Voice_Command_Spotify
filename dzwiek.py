2f809e694f4b2a5d090c98f32f1a80a819b87478
MichalTom4szewicz <37001951+MichalTom4szewicz@users.noreply.github.com>
2019-11-06 18:23:25 +0100

    Update dzwiek.py

--- dzwiek.py
+++ dzwiek.py
@@ -1,39 +1,15 @@
-from random import randint
-#import numpy as np
-
-
-from datetime import date, time
 import sounddevice as sd
-from scipy.io.wavfile import write
-
-from playsound import playsound
-
-import matplotlib.pyplot as plt
-import numpy as np
-import wave
-import sys
-import datetime
-
-import numpy as np
-import pyaudio
-
-import sounddevice as sd
-
-##############################################################################################################################################################################
-
 from pynput.keyboard import Key, Controller
 import time
 import speech_recognition as sr
 from pywinauto.application import Application
-import librosa
 import soundfile as sf
 import pywinauto
 import keyboard
-
+import os
 
 
 def rec_from_mic(recognizer, microphone):
-
     print("mow")
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
@@ -47,18 +23,12 @@ def rec_from_mic(recognizer, microphone):
 
 
 
-import os
 
 if __name__ == "__main__":
 
-    #app = Application().start(r"C:\Users\michal_internet\AppData\Roaming\Spotify\Spotify.exe")
-    #dlg_spec = app.window(title='Spotify Premium')
-    #time.sleep(10)
-    #dlg_spec.minimize()  # in production
-
-    fs = 44100
-    seconds=2.5
-    skok=7
+    fs = 16000
+    seconds = 2
+    skok = 7
 
     try:
         app = Application().connect(title='Spotify Premium')
@@ -67,53 +37,45 @@ if __name__ == "__main__":
         app = Application().start(r"C:\Users\michal_internet\AppData\Roaming\Spotify\Spotify.exe")
 
         for i in range(10):
-            print(i+1)
+            print(i + 1)
             time.sleep(1)
 
     handle = pywinauto.findwindows.find_windows(title='Spotify Premium')[0]
 
     window = app.window_(handle=handle)
 
-
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
     controller = Controller()
 
-    warunek=1
+    warunek = 1
     print('dziala:')
-    licznik=0
-    while(warunek == 1):
-        # q_pressed =0
-        #
-        # while(q_pressed==0):
-        if(keyboard.is_pressed('q')):
-            warunek=0
+    licznik = 0
+    while (warunek == 1):
+        if (keyboard.is_pressed('q')):
+            warunek = 0
 
-        licznik=licznik%10
-        print('mow:')
+        licznik = licznik % 10
         myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
         sd.wait()  # Wait until recording is finished
-        write(str(licznik)+'.wav', fs, myrecording)  # Save as WAV file
 
-        x, _ = librosa.load(str(licznik)+'.wav', sr=16000)
-        sf.write(str(licznik)+'.wav', x, 16000)
+        ty = sf.write(str(licznik) + '.wav', myrecording, 16000)
+        print(ty)
 
-        harvard = sr.AudioFile(str(licznik)+'.wav')
+        harvard = sr.AudioFile(str(licznik) + '.wav')
 
         with harvard as source:
             audio = recognizer.record(source)
         try:
-            tekst= recognizer.recognize_google(audio)
+            tekst = recognizer.recognize_google(audio)
             print('komenda:' + str(tekst))
         except sr.UnknownValueError:
-            print("nie rozpoznano"+ str(licznik))
-            tekst =""
+            # print("nie rozpoznano"+ str(licznik))
+            tekst = ""
 
+        #tekst = rec_from_mic(recognizer, microphone)
 
-        #qqtekst = rec_from_mic(recognizer, microphone)
-
-
-        if(tekst == 'next'):
+        if (tekst == 'next' or tekst == 'text' or tekst == 'ex' or tekst == 'neck' or tekst == 'max'):
             time.sleep(0.1)
             window.Maximize()
             controller.press(Key.ctrl_l)
@@ -122,7 +84,7 @@ if __name__ == "__main__":
             controller.release(Key.right)
             window.Minimize()
 
-        if (tekst == 'previous' or tekst =='envious' or tekst =='prev' or tekst =='pre'):
+        if (tekst == 'previous' or tekst == 'envious' or tekst == 'prev' or tekst == 'pre'):
             time.sleep(0.1)
             window.Maximize()
             controller.press(Key.ctrl_l)
@@ -131,7 +93,7 @@ if __name__ == "__main__":
             controller.release(Key.left)
             window.Minimize()
 
-        if (tekst == 'up' or tekst =='app' or tekst =='hop'):
+        if (tekst == 'up' or tekst == 'app' or tekst == 'hop'):
             time.sleep(0.1)
             window.Maximize()
             for i in range(skok):
@@ -141,7 +103,7 @@ if __name__ == "__main__":
                 controller.release(Key.up)
             window.Minimize()
 
-        if (tekst == 'down' or tekst =='own'):
+        if (tekst == 'down' or tekst == 'own'):
             time.sleep(0.1)
             window.Maximize()
             for i in range(skok):
@@ -151,17 +113,18 @@ if __name__ == "__main__":
                 controller.release(Key.down)
             window.Minimize()
 
-        if (tekst == 'start' or tekst =='stop' or tekst =='star' or tekst =='sta' or tekst =='art' or tekst =='op' or tekst =='top' ):
+        if (
+                tekst == 'start' or tekst == 'stop' or tekst == 'star' or tekst == 'sta' or tekst == 'art' or tekst == 'op' or tekst == 'top'):
             window.Maximize()
             time.sleep(0.1)
             controller.press(Key.space)
             controller.release(Key.space)
             window.Minimize()
 
-        if(tekst == 'exit' or tekst =='it' or tekst =='ex' or tekst =='exi' or tekst =='xit' or tekst =='exit '):
-            warunek=0
+        if (tekst == 'exit' or tekst == 'it' or tekst == 'exi' or tekst == 'xit' or tekst == 'exit ' or tekst == 'eat'):
+            warunek = 0
 
-        licznik+=1
+        licznik += 1
 
     window.Maximize()
     time.sleep(0.1)
